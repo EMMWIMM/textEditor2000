a236ec0af7f7037706ccde62c02a6a1ff27a5b6d
Emmalee <emilee.wimmer@gmail.com>
2022-11-29 16:46:46 -0700

    added id and content to be stored in put req

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,7 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {console.error('putDb not implemented');
+export const putDb = async (id, content) => {console.error('putDb not implemented');
   console.log('PUT to the database');
 // Create a connection to the database database and version we want to use.
 const contactDb = await openDB('jate', 1);
@@ -22,7 +22,7 @@ const tx = contactDb.transaction('jate', 'readwrite');
 
 const store = tx.objectStore('jate');
 
-const request = store.put({  });
+const request = store.put({ id: id, content: content  });
 
 const result = await request;
 console.log('🚀 - data saved to the database', result);
