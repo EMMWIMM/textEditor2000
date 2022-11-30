eab8964a9a75fee00742fe7e31c515dce660f483
Emmalee Anderson <emilee.wimmer@gmail.com>
2022-11-29 18:36:41 -0700

    checking over the todos

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,37 +12,39 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {console.error('putDb not implemented');
+// TODO: DONE? Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (id, content) => {
+  //console.error('putDb not implemented');
   console.log('PUT to the database');
-// Create a connection to the database database and version we want to use.
-const contactDb = await openDB('jate', 1);
+  // Create a connection to the database database and version we want to use.
+  const contactDb = await openDB('jate', 1);
 
-const tx = contactDb.transaction('jate', 'readwrite');
+  const tx = contactDb.transaction('jate', 'readwrite');
 
-const store = tx.objectStore('jate');
+  const store = tx.objectStore('jate');
 
-const request = store.put({ id: id, content: content  });
+  const request = store.put({ id: id, content: content  });
 
-const result = await request;
-console.log('🚀 - data saved to the database', result);
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () =>{ console.error('getDb not implemented');
+export const getDb = async () =>{
+  //console.error('getDb not implemented');
   console.log('GET from the database');
 
-const contactDb = await openDB('jate', 1);
+  const contactDb = await openDB('jate', 1);
 
-const tx = contactDb.transaction('jate', 'readonly');
+  const tx = contactDb.transaction('jate', 'readonly');
 
-const store = tx.objectStore('jate');
+  const store = tx.objectStore('jate');
 
-const request = store.getAll();
+  const request = store.getAll();
 
-const result = await request;
-console.log('result.value', result);
-return result;
+  const result = await request;
+  console.log('result.value', result);
+  return result;
 };
 
 initdb();
